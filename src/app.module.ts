65f6f96db53fe72c25f24f982b58d046aceed9ba
Dhyon <dhyon.kpm@hotmail.com>
2022-06-07 01:47:23 -0300

    feat: adding categories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { PlayersModule } from './players/players.module';
       'mongodb+srv://dhyon:1234@cluster0.ksctfjw.mongodb.net/smartranking?retryWrites=true&w=majority',
     ),
     PlayersModule,
+    CategoriesModule,
   ],
   controllers: [],
   providers: [],
